3e3b11ca5a939fde8fd7e6ede2acab51ec137a12
AWebZen <30926788+AWebZen@users.noreply.github.com>
2018-01-22 17:02:35 +0000

    Update projet.R

--- projet.R
+++ projet.R
@@ -23,7 +23,7 @@ print(colSums(gr1_adj))
 degrees = degree(graph1) 
 print(degrees)
 
-g2 <- graph( edges=c(1,2, 2,3, 3,1, 1,4, 4,2,2,4, 3,2, 3,4,4,3), n=10 )
+g2 <- graph( edges=c(1,2, 2,3, 3,1, 1,4, 4,2,2,4, 3,2, 3,4,4,3,5,7,5,6,6,7,5,8), n=10 )
 gr2_adj <- as.matrix(as_adj(g2)) #matrice d'adjacence, ligne = entree, colonne = sortie
 print(rowSums(gr2_adj))
 print(colSums(gr2_adj))
@@ -32,13 +32,19 @@ print(degree(g2, mode = c("out")))
 print(degree(g2, mode = c("in")))
 print(degree(g2))
 
+#Ajouter distribution noeuds
+
+
 #Verifier si graphe oriente ou pas
 symetric = isSymmetric.matrix(gr1_adj)
 
 #Takes a graph and a boolean as input and gives the degree, and if True, the degree in/out.
-degrees = function(gr, symetric)
+degrees = function(gr, symetric, gr_adj = "")
 {
-  gr_adj <- as.matrix(as_adj(gr))
+  if (! is.matrix(gr_adj))
+  {
+    gr_adj <- as.matrix(as_adj(gr))
+  }
   if (! symetric)
   {
     return(rbind(colSums(gr_adj) + rowSums(gr_adj), colSums(gr_adj), rowSums(gr_adj))) #global, in, out
@@ -49,52 +55,57 @@ degrees = function(gr, symetric)
   }
 }
 
-#local clustering coefficient
+#Prend une matrice d'adjacence et la rend symetrique
+undirect_graph = function(gr_adj)
+{
+  undir = gr_adj + t(gr_adj)
+  undir[which(undir > 1)] = 1
+  return(undir)
+}
+
+#local clustering coefficient - GLOBAL A FAIRE
 clustering_coeff = function(gr, symetric)
 {
   gr_adj <- as.matrix(as_adj(gr))
   diag(gr_adj) <- 0 #Pas d'aretes sur lui meme
   cl = c()
   V = 1:dim(gr_adj)[1]
-  if (symetric)
+  if (! symetric)
   {
-    deg = degrees(gr, symetric)
-    for (node in V)
-    {
-      if (deg[node] < 2) #NaN
-      {
-        cl = c(cl, 0)
-      }
-      else
-      {
-        node.voisins = which(gr_adj[node,] == 1)
-        aretes.voisins = sum(gr_adj[node.voisins, which(gr_adj[node,] == 1)])/2 # matrice d'adjacence est symetrique donc il faut diviser par 2 le resultat
-        cl = c(cl, aretes.voisins * 2/(deg[node] * (deg[node]-1))) 
-      }
-    }
+    gr_adj = undirect_graph(gr_adj)
   }
-  else
+  
+  deg = degrees(gr, TRUE, gr_adj)
+  for (node in V)
   {
-    deg = degrees(gr, symetric)[1,]
-    for (node in V)
-    {
-      print(node)
-      if (deg[node] < 2)
-      {
-        cl = c(cl, 0)
-      }
-      else
-      {
-        node.voisins = which(gr_adj[node,] == 1| gr_adj[,node] == 1)
-        print (paste("voisins", node.voisins))
-        aretes.voisins = sum(gr_adj[node.voisins, which(gr_adj[node,] == 1 | gr_adj[,node] == 1)]) # matrice d'adjacence est non symetrique
-        print (gr_adj[node.voisins, which(gr_adj[node,] == 1 | gr_adj[,node] == 1)])
-        print(paste("aretes voisins", aretes.voisins))
-        cl = c(cl, aretes.voisins/(deg[node] * (deg[node]-1)))
-      }
-    }
+    print(paste("node", node))
+    node.voisins = which(gr_adj[node,] == 1)
+    print (paste("voisins", node.voisins))
+    aretes.voisins = sum(gr_adj[node.voisins, which(gr_adj[node,] == 1)])/2 # matrice d'adjacence est symetrique donc il faut diviser par 2 le resultat
+    print (gr_adj[node.voisins, which(gr_adj[node,] == 1)])
+    print(paste("aretes voisins", aretes.voisins))
+    cl = c(cl, aretes.voisins * 2/(deg[node] * (deg[node]-1))) 
   }
-  retour <- c(cl,mean(cl))
-  names(retour) <- c(V, "global")
+
+  # else
+  # {
+  #   deg = degrees(gr, symetric)[1,]
+  #   for (node in V)
+  #   {
+  #     print(node)
+  #       node.voisins = which(gr_adj[node,] == 1| gr_adj[,node] == 1)
+  #       print (paste("voisins", node.voisins))
+  #       aretes.voisins = sum(gr_adj[node.voisins, which(gr_adj[node,] == 1 | gr_adj[,node] == 1)]) # matrice d'adjacence est non symetrique
+  #       print (gr_adj[node.voisins, which(gr_adj[node,] == 1 | gr_adj[,node] == 1)])
+  #       print(paste("aretes voisins", aretes.voisins))
+  #       cl = c(cl, aretes.voisins/(deg[node] * (deg[node]-1)))
+  #   }
+  # }
+  retour <- cl#c(cl, mean(cl, na.rm = TRUE))
+  # names(retour) <- c(V, "global")
   return (retour)
 }
+
+#Verification
+print(clustering_coeff(g2, FALSE))
+print (transitivity(as.undirected(g2, "collapse"), "local"))

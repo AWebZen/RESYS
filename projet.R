354a99da05bc0d27564d8b3ee6e160113a6a15c7
Clotildegarrido <35256972+Clotildegarrido@users.noreply.github.com>
2018-01-24 11:29:39 +0000

    Update projet.R

--- projet.R
+++ projet.R
@@ -79,10 +79,10 @@ clustering_coeff = function(gr, symetric)
   for (node in V)
   {
     print(paste("node", node))
-    node.voisins = which(gr_adj[node,] == 1)
+    node.voisins = which(gr_adj[node,] != 0)
     print (paste("voisins", node.voisins))
-    aretes.voisins = sum(gr_adj[node.voisins, which(gr_adj[node,] == 1)])/2 # matrice d'adjacence est symetrique donc il faut diviser par 2 le resultat
-    print (gr_adj[node.voisins, which(gr_adj[node,] == 1)])
+    aretes.voisins = sum(gr_adj[node.voisins, which(gr_adj[node,] != 0)])/2 # matrice d'adjacence est symetrique donc il faut diviser par 2 le resultat
+    print (gr_adj[node.voisins, which(gr_adj[node,] != 0)])
     print(paste("aretes voisins", aretes.voisins))
     cl = c(cl, aretes.voisins * 2/(deg[node] * (deg[node]-1))) 
   }
@@ -93,10 +93,10 @@ clustering_coeff = function(gr, symetric)
   #   for (node in V)
   #   {
   #     print(node)
-  #       node.voisins = which(gr_adj[node,] == 1| gr_adj[,node] == 1)
+  #       node.voisins = which(gr_adj[node,] != 0| gr_adj[,node] != 0)
   #       print (paste("voisins", node.voisins))
-  #       aretes.voisins = sum(gr_adj[node.voisins, which(gr_adj[node,] == 1 | gr_adj[,node] == 1)]) # matrice d'adjacence est non symetrique
-  #       print (gr_adj[node.voisins, which(gr_adj[node,] == 1 | gr_adj[,node] == 1)])
+  #       aretes.voisins = sum(gr_adj[node.voisins, which(gr_adj[node,] != 0 | gr_adj[,node] != 0)]) # matrice d'adjacence est non symetrique
+  #       print (gr_adj[node.voisins, which(gr_adj[node,] != 0 | gr_adj[,node] != 0)])
   #       print(paste("aretes voisins", aretes.voisins))
   #       cl = c(cl, aretes.voisins/(deg[node] * (deg[node]-1)))
   #   }
@@ -109,3 +109,99 @@ clustering_coeff = function(gr, symetric)
 #Verification
 print(clustering_coeff(g2, FALSE))
 print (transitivity(as.undirected(g2, "collapse"), "local"))
+
+#Pour oriente ou non, on regarde les degrees out
+BFS = function(gr_adj, s)
+{
+  V = dim(gr_adj)[1]
+  dist=(rep(Inf, V))
+  dist[s]=0
+  prev=(rep(-1,V))
+  Q=c(s)
+  while(length(Q)!=0)
+  {
+    u=Q[length(Q)]
+    Q=Q[-length(Q)]
+    for (v in which(gr_adj[u,]!=0))
+    {
+      if(dist[v]==Inf)
+      {
+        Q =c(Q, v)
+        dist[v]=dist[u]+1
+        prev[v]=u
+      }
+    }
+  }
+  return(rbind(dist, prev))
+}
+
+traceback=function(dist,prev)
+{
+  chemins = list()
+  s=which(dist==0)
+  for (u in c(1:length(dist))[-s])
+  {
+    local = c(u)
+    while(prev[u] != -1)
+    {
+      local = c(local, prev[u])
+      u = prev[u]
+    }
+    local = rev(local)
+    chemins[[length(chemins) +1]] = local
+  }
+  return(chemins)
+}
+S = BFS(gr1_adj,8)
+traceback(S[1,], S[2,])
+
+all_BFS=function(gr_adj)
+{
+  S_all = list()
+  tr_all = list()
+  for(s in 1:dim(gr_adj)[1])
+  {
+    S = BFS(gr_adj, s)
+    tr = traceback(S[1,], S[2,])
+    S_all[[length(S_all) +1]] = S
+    tr_all[[length(tr_all) +1]] = tr
+  }
+  return(list(S_all,tr_all))
+}
+
+Betweenness=function(tr_all)
+{
+  B=(rep(0,len))
+}
+
+
+#import the sample_weighted_adjmatrix file from bottom of the page:
+dat=read.csv(file.choose(),header=TRUE,row.names=1,check.names=FALSE) # read .csv file
+m=as.matrix(dat)
+net=graph.adjacency(m,mode="undirected",weighted=TRUE,diag=FALSE) #here is the first difference from the previous plot: we designate weighted=TRUE
+
+#Pour oriente ou non, on regarde les degrees out
+floyd_warshall = function(gr_adj)
+{
+  V = dim(gr_adj)[1]
+  gr_adj[gr_adj == 0] = Inf
+  diag(gr_adj) = 0
+  Next = t(matrix(1:V, V, V))
+  print(dim(Next))
+  print(Next)
+  for(k in 1:V)
+  {
+    for(i in 1:V)
+    {
+      for(j in 1:V)
+      {
+        if (gr_adj[i,j] > gr_adj[i,k]+gr_adj[k,j])
+        {
+          gr_adj[i,j] = gr_adj[i,k]+gr_adj[k,j]
+          Next[i,j] = Next[i,k]
+        }
+      }
+    }
+  }
+  return(list(gr_adj,Next))
+}

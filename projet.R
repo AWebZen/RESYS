2f2fab48765312762b088c6518ccdb7f1d5b6406
Clotildegarrido <35256972+Clotildegarrido@users.noreply.github.com>
2018-01-26 15:23:26 +0100

    Update projet.R

--- projet.R
+++ projet.R
@@ -293,6 +293,114 @@ longest_shortest_path_fw = function(distance)
 }
 
 
+PlCC=function(gr_adj)
+{
+    dist=gr_adj
+    dist[which(dist == 0)] = Inf
+    diag(dist)=0
+    d=dim(dist)[1]
+    k=1
+    PCC=list()
+    pcc=list()
+    for (x in 1:d)
+    {
+        for(y in 1:d)
+        {
+            if (dist[x,y]==k )
+            {
+                pcc=rbind(pcc,c(x,y))
+            }
+        }
+    }
+    PCC[[length(PCC)+1]]=pcc
+    while(length(which(dist == Inf))!=0 && length(pcc)!=0)
+    {
+        pcc=list()
+        for (u in 1:d)
+        {
+            for (v in 1:d)
+            {
+                if (dist[u,v]==k)
+                {
+                    for (w in c(1:d))
+                    {
+                        if (dist[v,w]==1 && w!=u && w!=v)
+                        {
+                            for (i in 1:dim(PCC[[k]])[1])
+                            {
+                                if ((PCC[[k]][i,1])==u  && (PCC[[k]][i,length(PCC[[k]][i,])])==v && w%in%PCC[[k]][i,]==FALSE)
+                                {
+                                    if (dist[u,w]==k+1|dist[u,w]==Inf)
+                                    {
+                                        pcc=rbind(pcc,c(PCC[[k]][i,],w))
+                                        dist[u,w]=k+1
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        if (length(pcc)!=0)
+        {
+            PCC[[length(PCC)+1]]=pcc
+        }
+        k=k+1
+    }
+    return(list(PCC,dist))
+}
+
+
+Betweenness=function(List_chemins,dist,n,Sym=TRUE)
+{
+    B=list()
+    for (x in 1:n)
+    {
+    B[length(B)+1]=list(matrix(0,n,n))
+    }
+    Tot=matrix(0,n,n)
+    for (i in 2:length(List_chemins[[1]]))
+    {
+        for (j in 1:dim(List_chemins[[1]][[i]])[1])
+        {
+            for (x in 3:length(List_chemins[[1]][[i]][j,])-1)
+            {
+                v=as.numeric(List_chemins[[1]][[i]][j,x])
+                s=as.numeric(List_chemins[[1]][[i]][j,1])
+                t=as.numeric(List_chemins[[1]][[i]][j,length(List_chemins[[1]][[i]][j,])])
+                B[[v]][s,t]=B[[v]][s,t]+1
+            }
+        }
+    }
+    for (i in 1:length(B))
+    {
+        Tot=Tot+(B[[i]])
+    }
+    for (i in 1:n)
+    {
+        for (j in 1:n)
+        {
+            Tot[i,j]=Tot[i,j]/(dist[[1]][i,j]-1)
+        }
+    }
+    b=list()
+    for (k in 1:n)
+    {
+        B[[k]]=B[[k]]/Tot
+        for (i in 1:n)
+        {
+            B[[k]][i,][which(is.na(B[[k]][i,]))]=0
+        }
+        if (Sym)
+        b=rbind(b,sum(B[[k]])/2)
+        else
+        b=rbind(b,sum(B[[k]]))
+    }
+    return(b)
+}
+
+
 #Tests varies des fonctions, verifications par igraph
 tests_varies = function()
 {
